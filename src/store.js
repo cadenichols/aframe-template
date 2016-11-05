46a9a5e7894d534c910ffded94c4b9e2c9dae5d8
Cade Nichols <c@codinghouse.co>
2016-11-04 19:46:00 -0700

    template ready

---
+++ src/store.js
@@ -0,0 +1,15 @@
+import { createStore, compose, applyMiddleware } from 'redux';
+import io from 'socket.io-client';
+import reducer from './reducers';
+
+const socket = io();
+
+const store = createStore(reducer, compose(
+  applyMiddleware(...middlewares)
+));
+
+socket.on('action', action => {
+  store.dispatch(action);
+});
+
+export default store;
